de1e2a87d5b5ec3a195670c1173c759e82a89404
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-04-14 22:55:45 +0800

    lex modify, add INCDEC(f), AND(f), OR(f), XOR(f), ADDSUB_ASSIGN(f)

---
+++ package/calculator_recursion/lex.h
@@ -0,0 +1,28 @@
+#ifndef __LEX__
+#define __LEX__
+
+#define MAXLEN 256
+
+// Token types
+typedef enum {
+    UNKNOWN, END, ENDFILE, 
+    INT, ID,
+    ADDSUB, MULDIV,
+    ASSIGN, 
+    LPAREN, RPAREN,
+    INCDEC,
+    AND, OR, XOR,
+    ADDSUB_ASSIGN
+} TokenSet;
+// things that missing: INCDEC(f), AND(f), OR(f), XOR(f), ADDSUB_ASSIGN(f)
+
+// Test if a token matches the current token 
+extern int match(TokenSet token);
+
+// Get the next token
+extern void advance(void);
+
+// Get the lexeme of the current token
+extern char *getLexeme(void);
+
+#endif // __LEX__
