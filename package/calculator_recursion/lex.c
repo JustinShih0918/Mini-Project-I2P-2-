57bef77b5f5789557bd4a7ef9e81357242979cac
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-04-18 00:59:44 +0800

    add error handle 1x (which is a invalid variable name)

--- package/calculator_recursion/lex.c
+++ package/calculator_recursion/lex.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 #include "lex.h"
+#include "parser.h"
 
 static TokenSet getToken(void);
 static TokenSet curToken = UNKNOWN;
@@ -15,9 +16,14 @@ TokenSet getToken(void)
     while ((c = fgetc(stdin)) == ' ' || c == '\t');
 
     if (isdigit(c)) { //INT
+
         lexeme[0] = c;
         c = fgetc(stdin);
         i = 1;
+        if(isalpha(c) || c == '_'){
+            error(NOTNUMID);
+            ungetc(c,stdin);
+        }
         while (isdigit(c) && i < MAXLEN) {
             lexeme[i] = c;
             ++i;
@@ -25,10 +31,10 @@ TokenSet getToken(void)
         }
         ungetc(c, stdin);
         lexeme[i] = '\0';
-        for(int j = 0;j<i;j++){
-            printf("%c",lexeme[j]);
-        }
-        printf("\n");
+        // for(int j = 0;j<i;j++){
+        //     printf("%c",lexeme[j]);
+        // }
+        // printf("\n");
         return INT;
     } else if (c == '+' || c == '-') { //ADDSUB, INCDEC, ADDSUB_ASSIGN
         lexeme[0] = c;
@@ -36,40 +42,40 @@ TokenSet getToken(void)
         if(c == lexeme[0]){ //INCDEC
             lexeme[1] = c;
             lexeme[2] = '\0';
-            printf("%c%c\n",lexeme[0],lexeme[1]);
+            //printf("%c%c\n",lexeme[0],lexeme[1]);
             return INCDEC;
         }
         else if(c == '='){ //ADDSUN_ASSIGN
             lexeme[1] = c;
             lexeme[2] = '\0';
-            printf("%c%c\n",lexeme[0],lexeme[1]);
+            //printf("%c%c\n",lexeme[0],lexeme[1]);
             return ADDSUB_ASSIGN;
         }
         else{
             ungetc(c,stdin); // ADDSUB
             lexeme[1] = '\0';
-            printf("%c\n",lexeme[0]);
+            //printf("%c\n",lexeme[0]);
             return ADDSUB;
         }
     } else if (c == '*' || c == '/') { // MULDIV
         lexeme[0] = c;
         lexeme[1] = '\0';
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return MULDIV;
     } else if (c == '\n') { //end
         lexeme[0] = '\0';
         return END;
     } else if (c == '=') { // assign
         strcpy(lexeme, "=");
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return ASSIGN;
     } else if (c == '(') { //Lparen
         strcpy(lexeme, "(");
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return LPAREN;
     } else if (c == ')') { // Rparen
         strcpy(lexeme, ")");
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return RPAREN;
     } else if (isalpha(c) || c == '_') { // ID
         lexeme[0] = c;
@@ -82,26 +88,26 @@ TokenSet getToken(void)
             c = fgetc(stdin);
         }
         ungetc(c,stdin);
-        for(int j = 0;j<i;j++){
-            printf("%c",lexeme[j]);
-        }
-        printf("\n");
+        // for(int j = 0;j<i;j++){
+        //     printf("%c",lexeme[j]);
+        // }
+        // printf("\n");
         lexeme[i] = '\0';
         return ID;
     } else if(c == '&'){ // and
         lexeme[0] = c;
         lexeme[1] = '\0';
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return AND;
     } else if(c == '|'){ // or
         lexeme[0] = c;
         lexeme[1] = '\0';
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return OR;
     } else if(c == '^'){ // xor
         lexeme[0] = c;
         lexeme[1] = '\0';
-        printf("%c\n",lexeme[0]);
+        //printf("%c\n",lexeme[0]);
         return XOR;
     }
     else if (c == EOF) { // end of file
